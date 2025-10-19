f745816215f65da4282285e709a4caf983e90074
Reindhart <japp.banjo@gmail.com>
2025-10-18 19:05:17 -0600

    GithubPages config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import path from 'path';
 
 export default defineConfig({
+  base: "/ActividadesSeminarioSO",
   plugins: [react(), tailwindcss(), TanStackRouterVite()],
   resolve: {
     alias: {
